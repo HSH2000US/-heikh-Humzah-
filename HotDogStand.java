4636990dc440605d8216e63098f86f367e3d0b0f
HSH2000US <57731842+HSH2000US@users.noreply.github.com>
2019-11-13 16:32:24 -0600

    Add files via upload

---
+++ HotDogStand.java
@@ -0,0 +1,18 @@
+
+
+// Humzah Hassan 
+
+// Declaring class 
+public class HotDogStand 
+{
+	
+
+    // Creating method to get the total count of hotdogs. 
+	public static double getHotDogCount (int hotdog)
+	{
+		
+	}
+	public static double 
+	
+
+}
---
+++ HotDogStandMain.java
@@ -0,0 +1,17 @@
+
+// Humzah Hassan main class for Hot Dog
+// Enter an import scanner
+import java.util.Scanner; 
+
+public class HotDogStandMain 
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner (System.in);
+
+		System.out.print("Enter amount of hotdogs: ");
+		 int hotDogsWanted = sc.nextInt();
+				
+	}
+}
+	
